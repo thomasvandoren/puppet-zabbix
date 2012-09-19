57e045dc403ee39aaa4f8f51b8d290af8eae4273
Thomas Van Doren <thomas.vandoren@gmail.com>
2012-09-18 21:35:10 -0700

    Convert report processor to use zabbix_sender binary.

--- lib/puppet/reports/zabbix.rb
+++ lib/puppet/reports/zabbix.rb
@@ -1,14 +1,13 @@
 require 'puppet'
-require 'base64'
-require 'json'
-require 'socket'
+require 'open4'
+require 'tempfile'
 
 Puppet::Reports.register_report(:zabbix) do
 
   config_file = File.join(File.dirname(Puppet.settings[:config]), "zabbix.yaml")
   raise(Puppet::ParseError, "Zabbix report config file #{config_file} not readable") unless File.exist?(config_file)
   config = YAML.load_file(config_file)
-  HOST, PORT = config[:zabbix_host], config[:zabbix_port]
+  HOST, PORT, ZABBIX_SENDER = config[:zabbix_host], config[:zabbix_port], config[:zabbix_sender]
 
   desc <<-DESC
   Send puppet run data to zabbix.
@@ -18,15 +17,15 @@ Puppet::Reports.register_report(:zabbix) do
     send_items_to_zabbix
   end
 
-  def connect
-    return TCPSocket.new(HOST, PORT)
+  def get_formatted_data
+    formatted_data = [format_one_line(self.host, 'puppet.run.timestamp', self.time.to_i),
+                      format_one_line(self.host, 'puppet.run.status',    self.status),
+                      format_one_line(self.host, 'puppet.run.time',      get_total_time),
+                      format_one_line(self.host, 'puppet.version',       self.puppet_version),]
+    return formatted_data.join("\n")
   end
 
-  def disconnect(connection)
-    connection.close unless connection.closed?
-  end
-
-  def total_time
+  def get_total_time
     # Returns the total time metric. Return 0.0 if the status is
     # failed.
     return 0.0 if self.status == 'failed'
@@ -35,87 +34,39 @@ Puppet::Reports.register_report(:zabbix) do
     }
   end
 
-  def data
-    # Return a list of objects that meet the zabbix item data spec.
-    clock = Time.now.to_i
-    return [{
-              :host  => self.host,
-              :key   => 'puppet.run.timestamp',
-              :value => self.time.to_i,
-              :clock => clock,
-            },
-            {
-              :host  => self.host,
-              :key   => 'puppet.run.status',
-              :value => self.status,
-              :clock => clock,
-            },
-            {
-              :host  => self.host,
-              :key   => 'puppet.run.time',
-              :value => total_time,
-              :clock => clock,
-            },
-            {
-              :host  => self.host,
-              :key   => 'puppet.version',
-              :value => self.puppet_version,
-              :clock => clock,
-            }]
+  # Returns the zabbix_sender command line call.
+  def get_zabbix_sender(zabbix_sender, host, port, input_file)
+    return "#{zabbix_sender} --zabbix-server #{host} --port #{port} --input-file #{input_file}"
   end
 
-  def request
-    # Return an object that matches the request data spec.
-    return {
-      :request => 'agent data',
-      :clock   => Time.now.to_i,
-      :data    => data
-    }
-  end
-
-  def zabbix_server_str
-    return "#{HOST}:#{PORT}"
+  def format_one_line(host, key, value)
+    return "#{host} #{key} #{value}"
   end
 
   def send_items_to_zabbix
-    # Send agent style data to zabbix server as specified in the
-    # sender protocol.
-    #
-    #   http://www.zabbix.com/documentation/1.8/protocols/agent
-    #
     begin
-      request_data = JSON.dump(request)
+      input_file = Tempfile.new('puppet.report.zabbix')
+      input_file.write(get_formatted_data)
 
-      # Get an open connection to the zabbix server.
-      s = connect
+      zabbix_sender_cmd = get_zabbix_sender(ZABBIX_SENDER, HOST, PORT, input_file.path)
 
-      # Write the request data as specified by the zabbix send
-      # protocol and flush the connection.
-      s.write 'ZBXD\x01'
-      s.write [request_data.size].pack('q')
-      s.write request_data
-      s.flush
-
-      # Get the response from the server.
-      header = s.read(5)
-      datalen = s.read(8).unpack('q').shift
-      raw_resp = s.read(datalen)
-
-      # Log data from zabbix server response.
-      resp = JSON.load(raw_resp)
-      response = resp.fetch('response')
-      Puppet.debug "Zabbix server responded with info: #{resp.fetch('info')}"
-      if response != 'success'
-        Puppet.warn "Zabbix server (#{zabbix_server_str}) responded with non-success response: #{response}"
-      else
-        Puppet.debug "Successfully sent puppet data to zabbix (#{zabbix_server_str}) for #{self.host}."
+      status = Open4::popen4(zabbix_sender_cmd) do |pid, stdin, stdout, stderr|
+        stdin.close
+        out_put = stdout.read.strip
+        err_put = stdout.read.strip
       end
-    rescue => e
-      Puppet.warn "Failed to send puppet data to zabbix (#{zabbix_server_str}) for #{self.host}."
+
+      Puppet.debug("Zabbix sender stderr: #{out_put}")
+      Puppet.debug("Zabbix sender stdout: #{out_put}")
+
+      if status.exitstatus == 0
+        Puppet.debug("Successfuly sent puppet data to zabbix for #{self.host}.")
+      else
+        Puppet.warn("Failed to send puppet data to zabbix (#{HOST}:#{PORT}) for #{self.host}")
+      end
     ensure
-      # Don't leave the connection open; it can adversely effect
-      # performance on the zabbix server.
-      disconnect(s)
+      input_file.close
+      input_file.unlink
     end
   end
 
